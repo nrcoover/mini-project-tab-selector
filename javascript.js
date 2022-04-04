0523829afd6ac812139dee50e38997b01fb9d9ba
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-04-04 14:12:41 -0400

    Dark Mode Toggle Fully Functional
    Implemented JavaScript to fully automate the color changes between Dark Mode and Light Mode.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,24 @@
 
         // global variable declerations
-        var tabs = document.getElementsByClassName('tab');
+        var lightBase = '#bdbdbd';
+        var lightBackground = '#FFFFFF';
+        var lightShadow = 'rgba(18, 18, 18, .3)';
+        var lightPrimary = '#6200EE';
+        var lightPrimaryDark = '#3700B3';
+        var lightSecondary = '#03DAC6';
+        var lightSecondaryDark = '#018786';
+        var lightErrorColor = '#B00020';
+        /* DARK THEME COLORS */
+        var darkBase = '#1d1d1d';
+        var darkBackground = '#121212';
+        var darkShadow = 'rgba(255, 255, 255, .1)'
+        var darkPrimary = '#BB86FC';
+        var darkPrimaryDark = '#3700B3';
+        var darkSecondary = '#03DAC6';
+        var darkErrorColor = '#CF6679';
 
-        
         // function to remove .show-active class & add to clicked
+        var tabs = document.getElementsByClassName('tab');
         function setNewActive(el) {
             // select all .content-body
             var contentBodies = document.getElementsByClassName('content-body');
@@ -14,7 +29,6 @@
             // add .show-active (back) to clicked
             document.getElementById(el.textContent.trim()).classList.add('show-active');
 
-            var tabs = document.getElementsByClassName('tab');
             for (var tab of tabs) {
                 tab.classList.remove('tab-active');
             }
@@ -23,11 +37,23 @@
 
         function turnOnDarkModeColors() {
             if (darkModetoggleIsOn === true) {
-                console.log("HE HE HE");
-                document.documentElement.style.setProperty('--light-theme-base', '#1d1d1d');
+                document.body.style.transition = 'color 1s, background-color 1s';
+                document.documentElement.style.setProperty('--light-theme-base', darkBase);
+                document.documentElement.style.setProperty('--light-theme-background', darkBackground);
+                document.documentElement.style.setProperty('--light-theme-primary', darkPrimary);
+                document.documentElement.style.setProperty('--light-theme-primary-dark', darkPrimaryDark);
+                document.documentElement.style.setProperty('--light-theme-secondary', darkSecondary);
+                document.documentElement.style.setProperty('--light-theme-secondary-dark', darkSecondary);
+                document.body.style.color = lightBackground;
             } else {
-                console.log("SHE SHE SHE");
-                document.documentElement.style.setProperty('--light-theme-base', '#bdbdbd');
+                document.body.style.transition = 'color 1s, background-color 1s';
+                document.documentElement.style.setProperty('--light-theme-base', lightBase);
+                document.documentElement.style.setProperty('--light-theme-background', lightBackground);
+                document.documentElement.style.setProperty('--light-theme-primary', lightPrimary);
+                document.documentElement.style.setProperty('--light-theme-primary-dark', lightPrimaryDark);
+                document.documentElement.style.setProperty('--light-theme-secondary', lightSecondary);
+                document.documentElement.style.setProperty('--light-theme-secondary-dark', lightSecondaryDark);
+                document.body.style.color = 'black';
             }
         }
 
@@ -64,7 +90,7 @@
         for (var tab of tabs) {
             // add click listener to each tab
             tab.addEventListener('click', function(e) {
-              setNewActive(e.currentTarget);
-              console.log(e.currentTarget);
+                setNewActive(e.currentTarget);
+                console.log(e.currentTarget);
             });
         }
