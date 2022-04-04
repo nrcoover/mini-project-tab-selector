dd99d84fe48a21d29922e323974101af0617c62c
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-04-04 13:03:48 -0400

    Adding Toggle Functionality
    Created JavaScript to made the Toggle Buttons actually toggle. NOTE: the changes they are meant to implement have yet to be programmed.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
-        // function to remove .show-active class & add to clicked
-        console.log('hello');
+
+        // global variable declerations
+        var tabs = document.getElementsByClassName('tab');
+
         
+        // function to remove .show-active class & add to clicked
         function setNewActive(el) {
-            var contentBodies = document.getElementsByClassName('content-body');
             // select all .content-body
+            var contentBodies = document.getElementsByClassName('content-body');
             // remove .show-active
-            console.log(contentBodies);
             for (var contentBody of contentBodies) {
                 contentBody.classList.remove('show-active');
             }
@@ -19,10 +21,34 @@
             el.classList.add('tab-active');
         }
 
-        // select .tab class, and loop through
-        var tabs = document.getElementsByClassName('tab');
-        for (var tab of tabs) {
+        // DARK MODE TOGGLE FUNCTIONALITY
+        var darkModeSwitch = document.querySelector('#Dark-Mode-Switch');
+        var darkModetoggleIsOn = false;
+        darkModeSwitch.addEventListener('click', function() {
+            if (darkModetoggleIsOn === false) {
+                darkModeSwitch.classList.add('toggle-is-active');
+                return darkModetoggleIsOn = true;
+            } else {
+                darkModeSwitch.classList.remove('toggle-is-active');
+                return darkModetoggleIsOn = false;
+            }
+        });
 
+        // LANGUAGE TOGGLE FUNCTIONALITY
+        var languageSwitch = document.querySelector('#Language-Switch');
+        var languagetoggleIsOn = false;
+        languageSwitch.addEventListener('click', function() {
+            if (languagetoggleIsOn === false) {
+                languageSwitch.classList.add('toggle-is-active');
+                return languagetoggleIsOn = true;
+            } else {
+                languageSwitch.classList.remove('toggle-is-active');
+                return languagetoggleIsOn = false;
+            }
+        });
+
+        // select .tab class, and loop through
+        for (var tab of tabs) {
             // add click listener to each tab
             tab.addEventListener('click', function(e) {
               setNewActive(e.currentTarget);
